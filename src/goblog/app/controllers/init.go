a72234891acaec23071a6210005872a15b2bbf1c
twan65 <cx.twan65@gmail.com>
2020-10-07 19:00:52 +0900

    GORMのため、テーブル削除及びインタセプター登録

--- src/goblog/app/controllers/init.go
+++ src/goblog/app/controllers/init.go
@@ -1,36 +1,38 @@
 package controllers
 
 import (
-	db "github.com/revel/modules/db/app"
 	"github.com/revel/revel"
 )
 
 // アプリケーション起動時、DBを初期化
-func InitDB() {
-	db.Init()
-	schema := `
-		CREATE TABLE IF NOT EXISTS posts (
-			id INTEGER PRIMARY KEY AUTOINCREMENT,
-			title TEXT	NOT NULL,
-			body TEXT	NOT NULL,
-			created_at DATETIME	NOT NULL,
-			updated_at DATETIME	NOT NULL
-		);
+// func InitDB() {
+// 	db.Init()
+// 	schema := `
+// 		CREATE TABLE IF NOT EXISTS posts (
+// 			id INTEGER PRIMARY KEY AUTOINCREMENT,
+// 			title TEXT	NOT NULL,
+// 			body TEXT	NOT NULL,
+// 			created_at DATETIME	NOT NULL,
+// 			updated_at DATETIME	NOT NULL
+// 		);
 
-		CREATE TABLE IF NOT EXISTS comments (
-			id          INTEGER PRIMARY KEY AUTOINCREMENT,
-			body        TEXT                NOT NULL,
-			commenter   TEXT                NOT NULL,
-			post_id     INT                 NOT NULL,
-			created_at  DATETIME            NOT NULL,
-			updated_at  DATETIME            NOT NULL
-		);
-	`
+// 		CREATE TABLE IF NOT EXISTS comments (
+// 			id          INTEGER PRIMARY KEY AUTOINCREMENT,
+// 			body        TEXT                NOT NULL,
+// 			commenter   TEXT                NOT NULL,
+// 			post_id     INT                 NOT NULL,
+// 			created_at  DATETIME            NOT NULL,
+// 			updated_at  DATETIME            NOT NULL
+// 		);
+// 	`
 
-	db.Db.Exec(schema)
-}
+// 	db.Db.Exec(schema)
+// }
 
 func init() {
 	// アプリケーション起動時、DBを初期化
 	revel.OnAppStart(InitDB)
+	revel.InterceptMethod((*GormController).Begin, revel.BEFORE)
+	revel.InterceptMethod((*GormController).Commit, revel.AFTER)
+	revel.InterceptMethod((*GormController).Rollback, revel.FINALLY)
 }
