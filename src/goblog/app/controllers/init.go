b990ab5955d0f22de20ff8990a3044ee89b5f773
twan65 <cx.twan65@gmail.com>
2020-10-10 16:47:05 +0900

    コメント処理削除

--- src/goblog/app/controllers/init.go
+++ src/goblog/app/controllers/init.go
@@ -4,31 +4,6 @@ import (
 	"github.com/revel/revel"
 )
 
-// アプリケーション起動時、DBを初期化
-// func InitDB() {
-// 	db.Init()
-// 	schema := `
-// 		CREATE TABLE IF NOT EXISTS posts (
-// 			id INTEGER PRIMARY KEY AUTOINCREMENT,
-// 			title TEXT	NOT NULL,
-// 			body TEXT	NOT NULL,
-// 			created_at DATETIME	NOT NULL,
-// 			updated_at DATETIME	NOT NULL
-// 		);
-
-// 		CREATE TABLE IF NOT EXISTS comments (
-// 			id          INTEGER PRIMARY KEY AUTOINCREMENT,
-// 			body        TEXT                NOT NULL,
-// 			commenter   TEXT                NOT NULL,
-// 			post_id     INT                 NOT NULL,
-// 			created_at  DATETIME            NOT NULL,
-// 			updated_at  DATETIME            NOT NULL
-// 		);
-// 	`
-
-// 	db.Db.Exec(schema)
-// }
-
 func init() {
 	// アプリケーション起動時、DBを初期化
 	revel.OnAppStart(InitDB)
